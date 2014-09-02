1079d0bd2a395571a595b40583644416d23d338c
Ottomen <ottomen@mail.ru>
2014-09-02 13:54:12 +0300

    add gallery slide feature up/down, add slider/footer sprites, <time> tag

--- assets/themeclubcube-design/js/loader.js
+++ assets/themeclubcube-design/js/loader.js
@@ -1,25 +1,38 @@
 (function($) {
+	var $body = $('body');
 	var $loaderWrap = $('.loader-container');
 	var $loader = $('.loader');
 	var $tiles = $loader.children();
 	var animate = true;
 	var current = 0;
+	var currentTime = new Date();
+	var miliseconds = 0;
+	var oldMiliseconds = 0;
 
-	console.log('Start', $tiles);
+	console.log('Start');
 
+	$body.addClass('ov-hidd');
+
+	
 	window.loaderInterval = setInterval(function() {
-		console.log(current, $tiles[current]);
-		// $tiles.removeClass('.active');
-		$($tiles[current]).addClass('.active');
+		// console.timeEnd('one');
+		oldMiliseconds = miliseconds - oldMiliseconds;
+		$tiles.removeClass('active');
+		$($tiles[current]).addClass('active');
 		current++;
 		if (current > 3) {
 			current = 0;
 		};
+		// console.time('one');
+		currentTime = new Date();
+		miliseconds = currentTime.getMilliseconds();
+		console.log(oldMiliseconds);
 	}, 300);
 
 	$(window).load(function() {
 		clearInterval(window.loaderInterval);
 		console.log('Loaded');
+		$body.removeClass('ov-hidd');
 		$loaderWrap.fadeOut(1000);
 	});
 })(jQuery);
