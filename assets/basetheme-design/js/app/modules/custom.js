b888a6618485292407e1100f3b742c1ab7a74d43
Roman L <r.maslakov@makebecool.com>
2016-02-18 11:29:32 +0200

    add fixeds

--- assets/basetheme-design/js/app/modules/custom.js
+++ assets/basetheme-design/js/app/modules/custom.js
@@ -116,10 +116,10 @@ appMakeBeCool.gateway.addClass('Custom', function (properties, $, $window, $docu
         },
 
         _parallaxStart = function () {
-           //$.stellar({
-           //    horizontalScrolling: false,
-           //    verticalOffset: 100
-           // })
+            _globals.prlxBack.stellar({
+               horizontalScrolling: false,
+               verticalOffset: 100
+            })
         },
 
         _setCustomMethods = function () {
