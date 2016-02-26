2b598befe6f4aec810d1d80227dbca73f25247de
Roman L <r.maslakov@makebecool.com>
2016-02-26 09:27:49 +0200

    add fixed js

--- assets/basetheme-design/js/app/modules/custom.js
+++ assets/basetheme-design/js/app/modules/custom.js
@@ -124,21 +124,21 @@ appMakeBeCool.gateway.addClass('Custom', function (properties, $, $window, $docu
                                     })
                                 },
                             });
-                            //setTimeout(function () {
-                            //    var topPoint = 0;
-                            //    topPoint = $('html,body').scrollTop(0);
-                            //    $('html, body').animate({
-                            //        opacity: 1,
-                            //    },{
-                            //        step: function (now, fx) {
-                            //            $(this).css({
-                            //                'transform': 'translate3d(0px,' + (topPoint) + 'px, 0px)',
-                            //                'transition': 'transform 0.7s ease-in',
-                            //            })
-                            //        },
-                            //        easening: 'linear',
-                            //    });
-                            //}, 200);
+                            setTimeout(function () {
+                                var topPoint = 0;
+                                topPoint = $('html,body').scrollTop(0);
+                                $('html, body').animate({
+                                    opacity: 1,
+                                },{
+                                    step: function (now, fx) {
+                                        $(this).css({
+                                            'transform': 'translate3d(0px,' + (topPoint) + 'px, 0px)',
+                                            'transition': 'transform 0.7s ease-in',
+                                        })
+                                    },
+                                    easening: 'linear',
+                                });
+                            }, 200);
                         }
                     });
                 }
