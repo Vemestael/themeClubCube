2a2a4eeb1d82b3cbbed4f599a551c79d040d9bcc
Roman L <r.maslakov@makebecool.com>
2016-03-02 09:21:17 +0200

    add fixed at dropmenu

--- assets/basetheme-design/Gulpfile.js
+++ assets/basetheme-design/Gulpfile.js
@@ -13,81 +13,85 @@ var imagemin = require('gulp-imagemin');
 
 gulp.task('serve', function () {
 
-    bs.init({
-        proxy: "http://localhost:63342/themeClubCube/assets/basetheme-design/"
-    });
+  bs.init({
+    proxy: "http://localhost:63342/assets/basetheme-design/",
+    port: 63342,
+    ui: {
+      port: 63342
+    }
+  });
 
-    gulp.watch("css/*.css").on('change', bs.reload);
-    gulp.watch("*.html").on('change', bs.reload);
+  gulp.watch("css/*.css").on('change', bs.reload);
+  gulp.watch("*.html").on('change', bs.reload);
 });
 
 gulp.task('sass', function (callback) {
-    return gulp.src(['css/sass/critical.sass', 'css/sass/main-style.sass'])
-        .pipe(plumber(
-            {
-                errorHandler: notify.onError(function (err) {
-                        return {
-                            title: 'sass',
-                            message: err.message
-                        };
-                    }
-                )
-            }))
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(debug({title: 'sass:'}))
-        .pipe(autoprefixer({
-            browsers: [
-                'Chrome >= 35',
-                'Firefox >= 31',
-                'Edge >= 12',
-                'Explorer >= 9',
-                'iOS >= 8',
-                'Safari >= 8',
-                'Android 2.3',
-                'Android >= 4',
-                'Opera >= 12'
-            ],
-            cascade: true
-        }))
-        .pipe(debug({title: 'prefx:'}))
-        .pipe(sourcemaps.write('.', {
-            includeContent: false,
-            sourceRoot: 'source'
-        }))
-        .pipe(debug({title: 'maps:'}))
-        .pipe(gulp.dest('css'));
-    callback();
+  return gulp.src(['css/sass/critical.sass', 'css/sass/main-style.sass'])
+      .pipe(plumber(
+          {
+            errorHandler: notify.onError(function (err) {
+                  return {
+                    title: 'sass',
+                    message: err.message
+                  };
+                }
+            )
+          }))
+      .pipe(sourcemaps.init())
+      .pipe(sass())
+      .pipe(debug({title: 'sass:'}))
+      .pipe(autoprefixer({
+        browsers: [
+          'Chrome >= 35',
+          'Firefox >= 31',
+          'Edge >= 12',
+          'Explorer >= 9',
+          'iOS >= 8',
+          'Safari >= 8',
+          'Android 2.3',
+          'Android >= 4',
+          'Opera >= 12'
+        ],
+        cascade: true
+      }))
+      .pipe(debug({title: 'prefx:'}))
+      .pipe(sourcemaps.write('.', {
+        includeContent: false,
+        sourceRoot: 'source'
+      }))
+      .pipe(debug({title: 'maps:'}))
+      .pipe(gulp.dest('css'));
+  callback();
 });
 
 gulp.task('templates', function (callback) {
-    gulp.src('jade/*.jade')
-        .pipe(plumber())
-        .pipe(jade({
-            pretty: true,
-        }))
-        .on('error', notify.onError(function (err) {
-            return {
-                title: 'jade',
-                message: err.message
-            }
-        }))
-        .pipe(gulp.dest('.'))
-        .pipe(debug({title: 'jade:'}));
-    callback();
+  gulp.src('jade/*.jade')
+      .pipe(plumber())
+      .pipe(jade({
+        pretty: true,
+      }))
+      .on('error', notify.onError(function (err) {
+        return {
+          title: 'jade',
+          message: err.message
+        }
+      }))
+      .pipe(gulp.dest('.'))
+      .pipe(debug({title: 'jade:'}));
+  callback();
 });
 
 gulp.task('minImg', function () {
-    return gulp.src('../uploads/gallery/*.jpg')
-        .pipe(imagemin({
-            progressive: true,
-        }))
-        .pipe(gulp.dest('../uploads/gallery/images'));
+  return gulp.src('../uploads/gallery/*.jpg')
+      .pipe(imagemin({
+        progressive: true,
+      }))
+      .pipe(gulp.dest('../uploads/gallery/images'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('css/sass/**/*.*', ['sass']);
-    gulp.watch('jade/**/*.*', ['templates']);
+  gulp.watch('css/sass/**/*.*', ['sass']);
+  gulp.watch('jade/**/*.*', ['templates']);
 });
 
 gulp.task('default', ['serve', 'templates', 'sass', 'watch']);
