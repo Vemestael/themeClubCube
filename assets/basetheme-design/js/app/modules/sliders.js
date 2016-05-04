3014ce5ce0e3c9625b243b2ff4cda870d2ba3dd4
SergeyLys <s.lysykh@makebecool.com>
2016-05-04 10:26:11 +0300

    fixed

--- assets/basetheme-design/js/app/modules/sliders.js
+++ assets/basetheme-design/js/app/modules/sliders.js
@@ -74,7 +74,7 @@ appMakeBeCool.gateway.addClass('Sliders', function (properties, $, $window, $doc
                 accessibility: false, //don't jumping the scroll in chrome!!!
                 dots: false,
                 infinite: false,
-                speed: 550,
+                speed: 330,
                 centerPadding: 30,
                 slidesToShow: 4,
                 slidesToScroll: 4,
