87085f50e0c78ffa585915687be3a15b648c4212
Valentin Shimshirt <v.shimshirt@gmail.com>
2016-10-07 15:18:00 +0300

    hotfix

--- assets/themeclubcube-elements/snippets/getTemplateValues.php
+++ assets/themeclubcube-elements/snippets/getTemplateValues.php
@@ -1,7 +1,12 @@
 <?php
 //добавить проверку на то, нужно ли вообще
 //добавлять класс
-$templateName = $modx->getObject('modTemplate', array('id' => $id))->get('templatename');
+try{
+	$templateName = $modx->getObject('modTemplate', $id)->get('templatename');	
+}catch(Exception $e){
+	return;
+}
+
 if ($currentChunck == 'eventsSquareItem' && ($templateName == 'indexEvents' || $templateName == 'indexFEvents' || $templateName == 'events'))return 'col-md-4';
 if ($currentChunck == 'eventsRectangleItem' && ($templateName == 'indexEvents' || $templateName == 'indexFEvents' || $templateName == 'events'))return 'col-lg-6';
 if ($currentChunck == 'eventsSquareList' && ($templateName != 'indexEvents' || $templateName != 'indexFEvents'))return 'col-sm-12';
